b7918ac26dadc5e96a1eb34662bbdd2426a124b9
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2024-03-11 13:32:48 -0300

    Add project files.

---
+++ LINQ - Leitura de CSV/Entities/Customer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ___Leitura_de_CSV.Entities
+{
+    internal class Customer
+    {
+        public string ID { get; set; }
+        public string CPF { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Income { get; set; }
+        public string Professional_Status { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {ID}, \n CPF: {CPF}.\n FullName: {First_Name} {Last_Name},\n " +
+                $"City: {City} - {State}.\n Birthdate: {BirthDate},\n Income: {Income},\n" +
+                $"Professional Status: {Professional_Status},\n Email: {Email},\nPhone: {Phone} ";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Customer customer &&
+                   ID == customer.ID &&
+                   CPF == customer.CPF &&
+                   Email == customer.Email;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ID, CPF, Email);
+        }
+
+        public Customer(
+            string iD,
+            string cPF,
+            string first_Name,
+            string last_Name,
+            string state,
+            string city,
+            DateTime birthDate,
+            double income,
+            string professional_Status,
+            string email,
+            string phone)
+        {
+            ID = iD;
+            CPF = cPF;
+            First_Name = first_Name;
+            Last_Name = last_Name;
+            State = state;
+            City = city;
+            BirthDate = birthDate;
+            Income = income;
+            Professional_Status = professional_Status;
+            Email = email;
+            Phone = phone;
+        }
+        public Customer() { }
+    }
+}
+
---
+++ LINQ - Leitura de CSV/Program.cs
@@ -0,0 +1,68 @@
+﻿using LINQ___Leitura_de_CSV.Entities;
+
+namespace LINQ___Leitura_de_CSV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Wesley\OneDrive\Área de Trabalho\sales_customers.csv";
+            List<Customer> customers = new List<Customer>();
+            List<String> treatedLines = new List<string>();
+
+
+
+            try
+            {
+
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!(sr.EndOfStream))
+                    {
+                        List<String> lines = sr.ReadLine().Split(",").ToList();
+                        treatedLines = eliminador(lines);
+                        customers.Add(new Customer
+                        {
+                            ID = treatedLines[0],
+                            CPF = treatedLines[1],
+                            First_Name = treatedLines[2],
+                            Last_Name = treatedLines[3],
+                            State = treatedLines[4],
+                            City = treatedLines[5],
+                            BirthDate = DateTime.Parse(treatedLines[6]),
+                            Income = double.Parse(treatedLines[7]),
+                            Professional_Status = treatedLines[8],
+                            Email = treatedLines[9],
+                            Phone = treatedLines[10]
+
+                        });
+                    }
+
+
+                }
+                Console.WriteLine("Verificar Income de usuários: ");
+                int verificar = int.Parse(Console.ReadLine());
+                customers.Sort((c,x)=> c.First_Name.CompareTo(x.First_Name));
+                foreach (var item in customers.Where(c => c.Income >= verificar).ToList())
+                {
+                    Console.WriteLine("=======================");
+                    Console.WriteLine(item);
+                };
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static List<String> eliminador(List<String> paraEliminar)
+        {
+            List<String> paraRetorno = new List<string>();
+            for (int i = 0; i < paraEliminar.Count; i++)
+            {
+                paraRetorno.Add(paraEliminar[i].Substring(1, paraEliminar[i].Length - 2));
+            }
+            return paraRetorno;
+        }
+    }
+}
